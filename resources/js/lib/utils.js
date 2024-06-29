637c9ab15b5958488105b90aae865e8adc427da7
HumaCode <humaidizakaria1998@gmail.com>
2024-06-29 12:42:08 +0700

    fix validasi pada name array tidak muncul

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -207,7 +207,12 @@ export class HandleFormSubmit extends AjaxOption{
                             let i = 0
                             for (let [key, value] of Object.entries(errors)) {
     
-                                const input = $(`[name="${key}"]`)
+                                let input = $(`[name="${key}"]`)
+
+                                if (!input.length) {
+                                    input = $(`[name="${key}[]"]`)
+                                }
+
                                 if (i === 0) {
                                     input.focus()
                                 }
